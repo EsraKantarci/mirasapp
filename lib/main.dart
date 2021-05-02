484490d0614b231fe3e704501c0dd9b35fbc46f2
Esra <av.esrakantarci@gmail.com>
2021-05-02 16:57:08 +0300

    Home page landing

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomePage(),
