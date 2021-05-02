a61e81f1f9cb70472b14c203280a115cd20e3291
Esra <av.esrakantarci@gmail.com>
2021-05-02 16:36:37 +0300

    Home page landing

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,30 @@
 import 'package:flutter/material.dart';
 
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black38,
+        body: Container(
+            child: SafeArea(
+                child: Column(
+          children: <Widget>[
+            Text(
+              "Uygulama Hakkında",
+              style: TextStyle(
+                fontFamily: "Avenir",
+                fontSize: 44,
+                color: Colors.blue,
+                fontWeight: FontWeight.w900,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ],
+        ))));
+  }
+}
