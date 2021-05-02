2e330205dc748123f889da6e30704c1c3af4f5fa
Esra <av.esrakantarci@gmail.com>
2021-05-02 16:39:09 +0300

    Home page landing

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 
 import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+// For readability purposes, home_page.dart is in another file.
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData.dark(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
