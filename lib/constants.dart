7baa9208b452e939886290d3821264710dd3e746
Esra <av.esrakantarci@gmail.com>
2021-05-02 16:58:57 +0300

    Constant variables for the references are added

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// Palette for the starter
+Color primaryTextColor = Color(0xFF414C6B);
+Color secondaryTextColor = Color(0xFFE4979E);
+Color titleTextColor = Colors.white;
+Color contentTextColor = Color(0xff868686);
+Color navigationColor = Color(0xFF6751B5);
+Color gradientStartColor = Color(0xFF0050AC);
+Color gradientEndColor = Color(0xFF9354B9);
+
+//Form page initializators
