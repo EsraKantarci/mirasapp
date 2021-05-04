63df3342d7be1679e817ca9c8e69787f83dd20b9
Esra <av.esrakantarci@gmail.com>
2021-05-04 23:53:31 +0300

    Form building with text field (step 1)

--- lib/view/start_page.dart
+++ lib/view/start_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:miras/model/constants.dart';
+import 'package:miras/view/home_page.dart';
+
+// https://www.youtube.com/watch?v=mLAY8sp-IoE
+// https://www.youtube.com/watch?v=H2pVgDjDrxQ
 
 class StartPage extends StatelessWidget {
   @override
@@ -26,52 +30,90 @@ class StartPage extends StatelessWidget {
               ],
             ),
           ),
-          SafeArea(child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-               Container(),
-                Container(
-                  child: Text(
-                    "Miras bırakanın ismi:",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: AppColors.mainColor,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 30,),
-                Container(
-                  child: TextField(
-                    onChanged: (text) {},
-                    decoration: InputDecoration(
-                      hintText: "Miras bırakanın ismini giriniz...",
-                      hintStyle: TextStyle(fontWeight: FontWeight.normal),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
+          SafeArea(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      "Miras bırakanın ismi:",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.mainColor,
                       ),
                     ),
                   ),
-                ),
-              ],
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.1,
+                    child: TextField(
+                      onChanged: (text) {},
+                      decoration: InputDecoration(
+                        hintText: "Miras bırakanın ismini giriniz...",
+                        hintStyle: TextStyle(fontWeight: FontWeight.normal),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    child: SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.1,
+                      width: MediaQuery.of(context).size.width,
+                      child: buildElevatedButton(context),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),),
+          ),
           Container(
             padding: EdgeInsets.symmetric(horizontal: 16),
             child: Column(
-              children: <Widget>[
-
-              ],
+              children: <Widget>[],
             ),
           ),
         ],
       ),
     );
+  }
+
+  ElevatedButton buildElevatedButton(BuildContext context) {
+    return ElevatedButton(
+                      child: Text('HESAPLAMAYA BAŞLA'),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => HomePage()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(50),
+                            ),
+                          ),
+                          padding: EdgeInsets.symmetric(vertical: 5),
+                          primary: Colors.white,
+                          onPrimary: AppColors.mainColor,
+                          textStyle: TextStyle(
+                              color: AppColors.mainColor,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold)),
+
+                    );
   }
 
   Column buildHeaderDetails() {
@@ -89,7 +131,7 @@ class StartPage extends StatelessWidget {
               )),
         ),
         SizedBox(height: 20),
-       ],
+      ],
     );
   }
 
