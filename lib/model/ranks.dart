0f2fdbb280e221adea9ec5706b8d6e9b31231b3e
Esra <av.esrakantarci@gmail.com>
2021-05-16 00:55:02 +0300

    wireframe mockups by balsamiq for the screenviews are added

---
+++ lib/model/ranks.dart
@@ -0,0 +1,15 @@
+class Rank {
+  int rank;
+  String relativeName;
+  double rate;
+  double hiddenRate;
+
+  Rank(
+      {this.rank = -1,
+      this.relativeName = '',
+      this.rate = 1,
+      this.hiddenRate = 1});
+
+}
+
+
